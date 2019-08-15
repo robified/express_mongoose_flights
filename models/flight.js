e3a30ff89550bcd60b96cf848c038dcb6587d73c
Robin <daorob@gmail.com>
2019-08-15 18:06:59 -0500

    Finish creating flightSchema.

---
+++ models/flight.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var flightSchema = new Schema({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United']
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999
+    },
+    departs: {
+        type: Dates
+        // default: function() { One year from date created
+        //     return
+        // }
+    }
+});
+
+module.exports = mongoose.model('Flight', flightSchema);
