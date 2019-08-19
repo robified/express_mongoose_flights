cc86cafe6387b27244a0dbf60d3c36070803a921
Robin <daorob@gmail.com>
2019-08-19 00:18:02 -0500

    Fix destination controller create method. Fix show.ejs to airport instead of destination and modified table header row.

--- routes/destination.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var destinationCtrl = require('../controllers/destination');
-
-router.post('/flights/:id/destinations', destinationCtrl.create);
-
-module.exports = router;
