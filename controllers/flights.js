2129635676c501647aeb6a24769951c6e86af061
Robin <daorob@gmail.com>
2019-08-20 05:45:44 -0500

    Update flights controller and added show function.

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,18 +1,22 @@
 var Flight = require('../models/flight');
+var Ticket = require('../models/ticket');
 
 module.exports = {
-    indexView,
-    newView,
+    index,
+    new: newFlight,
     create,
-    showView
+    show
 };
 
-function showView(req, res) {
-    Flight.findById(req.params.id, function(err, flight) {
-        res.render('flights/show', {
-            title: 'Flight Details',
-            flight
-        });
+function show(req, res) {
+    Flight.findById(req.params.id, function(err, flight,) {
+        Ticket.find({flight: flight._id}, function(err, tickets) {
+            res.render('flights/show', { 
+                title: 'Flight Details', 
+                flight, 
+                tickets
+            });
+        });  
     });
 };
 
@@ -27,11 +31,11 @@ function create(req, res) {
     });
 };
 
-function newView(req, res) {
-    res.render('flights/new', { title: 'Add Flight' });
+function newFlight(req, res) {
+    res.render('flights/new', { title: 'Add A Flight' });
 };
 
-function indexView(req, res) {
+function index(req, res) {
     Flight.find({}, function(err, flights) {
         res.render('flights/index', { title: 'All Flights', flights}); 
     });
