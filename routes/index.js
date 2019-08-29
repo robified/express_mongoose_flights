75db07a5b30867dfc409790da7c6abce90ea0d18
Robin <daorob@gmail.com>
2019-08-29 14:50:48 -0500

    Fi landing page to redirect to flights.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.redirect('/flights',  { title: 'Add Flight' });
+  res.redirect('/flights');
 });
 
 module.exports = router;
